0fc99fab400260c9e71161b5212bf1832721eb0e
lluni <36239317+lluni@users.noreply.github.com>
2025-10-26 13:54:34 +0100

    Fix boolean value from 'true' to 'True'

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def process(latest_version: Version):
 
     print("Downloading patches")
     pikoRelease = download_release_asset(
-        "crimera/piko", "^piko.*jar$", "bins", "patches.jar", true
+        "crimera/piko", "^piko.*jar$", "bins", "patches.jar", True
     )
 
     print("Downloading integrations")
